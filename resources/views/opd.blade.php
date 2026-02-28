8865ca7bb0577810871b1204537f95f60756672a
Adi Murdayani <adimurdayani@gmail.com>
2026-02-28 10:57:35 +0800

    added: tabel html

--- resources/views/opd.blade.php
+++ resources/views/opd.blade.php
@@ -9,7 +9,43 @@
         <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
             <div class="overflow-hidden bg-white shadow-sm dark:bg-gray-800 sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
-                    {{ __('Halaman OPD!') }}
+                    <div class="overflow-x-auto">
+                        <table class="w-full divide-y divide-gray-200 dark:divide-gray-700">
+                            <thead class="bg-gray-50 dark:bg-gray-700">
+                                <tr>
+                                    <th
+                                        class="px-6 py-3 text-xs font-medium text-left text-gray-500 uppercase dark:text-gray-300">
+                                        No
+                                    </th>
+                                    <th
+                                        class="px-6 py-3 text-xs font-medium text-left text-gray-500 uppercase dark:text-gray-300">
+                                        Nama
+                                    </th>
+                                    <th
+                                        class="px-6 py-3 text-xs font-medium text-left text-gray-500 uppercase dark:text-gray-300">
+                                        Email
+                                    </th>
+                                    <th
+                                        class="px-6 py-3 text-xs font-medium text-left text-gray-500 uppercase dark:text-gray-300">
+                                        Status
+                                    </th>
+                                    <th
+                                        class="px-6 py-3 text-xs font-medium text-center text-gray-500 uppercase dark:text-gray-300">
+                                        Aksi
+                                    </th>
+                                </tr>
+                            </thead>
+                            <tbody class="divide-y divide-gray-200 dark:divide-gray-700">
+                                <tr class="transition hover:bg-gray-50 dark:hover:bg-gray-700">
+                                    <td class="px-6 py-4 text-gray-700 dark:text-gray-200">1</td>
+                                    <td class="px-6 py-4 text-gray-700 dark:text-gray-200">Adi Murdayani</td>
+                                    <td class="px-6 py-4 text-gray-700 dark:text-gray-200">adi@email.com</td>
+                                    <td class="px-6 py-4">Aktif</td>
+                                    <td class="px-6 py-4 text-center">Edit</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
                 </div>
             </div>
         </div>
